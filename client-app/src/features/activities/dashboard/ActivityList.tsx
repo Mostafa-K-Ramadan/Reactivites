cc56b7e912fa5f6329edfb95edaf53b4e1122b50
Mostafa-K-Ramadan <mostafa1115@hotmail.com>
2021-07-01 05:05:54 +0300

    End of Section 6

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
+import { useState } from 'react';
 import { Button, Item, ItemContent, Label, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/Activity';
 
@@ -6,9 +7,17 @@ interface props {
     activities: Activity[];
     selectActivity: (id: string) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityList({activities, selectActivity,deleteActivity}: props){
+export default function ActivityList({activities, selectActivity,deleteActivity,submitting}: props){
+
+    const [target, setTarget] = useState('');
+
+    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string){
+        setTarget(e.currentTarget.name);
+        deleteActivity(id);
+    }
 
     return (
         <Segment>
@@ -24,7 +33,8 @@ export default function ActivityList({activities, selectActivity,deleteActivity}
                             </Item.Description>
                             <Item.Extra>
                                 <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='blue' />
-                                <Button onClick={() => deleteActivity(activity.id)} floated='right' content='Delete' color='red' />
+                                <Button name={activity.id}
+                                loading={submitting && target === activity.id} onClick={(e) => handleActivityDelete(e, activity.id)} floated='right' content='Delete' color='red' />
                                 <Label basic content={activity.category} />
                             </Item.Extra>
                         </ItemContent>
